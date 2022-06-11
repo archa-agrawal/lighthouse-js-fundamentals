b1933c8dceb3d52a18be8d749ed19b4f8450567f
Archana Agrawal <archana.agrawal3@outlook.com>
2022-06-11 05:45:58 -0400

    Add more conditional examples

--- conditional.js
+++ conditional.js
@@ -4,7 +4,15 @@ if (raining) {
   console.log("Don't forget your umbrella!");
 }
 
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
 const temperature = 12;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
