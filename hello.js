8cd7b42f7bbf7f602b3579cee001b2524945882b
Archana Agrawal <archana.agrawal3@outlook.com>
2022-06-11 05:51:55 -0400

    even.js

--- hello.js
+++ hello.js
@@ -2,6 +2,17 @@ const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
